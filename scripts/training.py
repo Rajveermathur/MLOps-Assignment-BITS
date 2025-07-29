46360efdba828c8b9c661329f4c3e0df351b3452
Rajveermathur <rajveer.mathur25@gmail.com>
2025-07-29 22:36:48 +0530

    Added more metrics

--- scripts/training.py
+++ scripts/training.py
@@ -3,7 +3,7 @@ import mlflow.sklearn
 from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
@@ -29,7 +29,9 @@ def train_and_log_model(model, name, params={}):
         mlflow.set_tag("model_type", name)
 
         # Log params
-        mlflow.log_params(params)
+        model.set_params(**params)
+        all_params = model.get_params()
+        mlflow.log_params(all_params)
 
         # Train
         model.fit(X_train, y_train)
@@ -38,10 +40,14 @@ def train_and_log_model(model, name, params={}):
         y_pred = model.predict(X_test)
 
         # Evaluate
-        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        rmse = root_mean_squared_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        
         mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mse", mse)
+        mlflow.log_metric("r2", r2)
 
-        # Log model
         mlflow.sklearn.log_model(model, "model", registered_model_name=name)
 
         print(f"🔍 {name} RMSE: {rmse:.4f}")
